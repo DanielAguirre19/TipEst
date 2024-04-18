0a82f6c6ffa591b60a1e3377e2016b15af3cd247
DanielAguirre19 <danielaguirrebravo@gmail.com>
2024-04-17 19:27:27 -0600

    FaltaPerfil

--- app/src/main/java/mx/unam/fi/tipest/ui/views/ImgPumaBusView.kt
+++ app/src/main/java/mx/unam/fi/tipest/ui/views/ImgPumaBusView.kt
@@ -1,17 +1,13 @@
 package mx.unam.fi.tipest.ui.views
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
