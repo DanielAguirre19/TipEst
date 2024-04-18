721ab9c9e93b4c031a28b839d85f9fc4caed7388
DanielAguirre19 <danielaguirrebravo@gmail.com>
2024-04-18 17:54:27 -0600

    Mapa V2 con DropMenu Completo

--- app/src/main/java/mx/unam/fi/tipest/maps/mapsList.kt
+++ app/src/main/java/mx/unam/fi/tipest/maps/mapsList.kt
@@ -3,13 +3,33 @@ package mx.unam.fi.tipest.maps
 import com.google.android.gms.maps.model.LatLng
 
 val coordenadas = listOf(
-    LatLng(19.33120996609985, -99.18396542264932),
-    LatLng(19.33481563792762, -99.18137174303882),
-    LatLng(19.33328389800478, -99.1804679145199)
+    LatLng(19.33082853354208, -99.1868514221428),
+    LatLng(19.324448596337234, -99.17883139700889),
+    LatLng(19.318819378624177, -99.17734169939595),
+    LatLng(19.324190679486854, -99.18504140017488),
+    LatLng(19.334681161799757, -99.18462629515773),
+    LatLng(19.335162030721943, -99.1830193644709),
+    LatLng(19.334920851441062, -99.18647503563511),
+    LatLng(19.33127191853024, -99.18414850494838),
+    LatLng(19.333299963170845, -99.17981161050116),
+    LatLng(19.329696862977293, -99.1767135896051),
+    LatLng(19.334744771810104, -99.18092113193325),
+    LatLng(19.33549316099372, -99.18822682029175),
+    LatLng(19.331276729824378, -99.18134593324835),
 )
 
 val nombresFac = listOf(
-    "Inge",
-    "Odonto",
-    "Medicina"
+    "Facultad de Arquitectura",
+    "Facultad de Ciencias",
+    "Facultad de Ciencias Políticas y Sociales",
+    "Facultad de Contaduría y Administración",
+    "Facultad de Derecho",
+    "Facultad de Economía",
+    "Facultad de Filosofía y Letras",
+    "Facultad de Ingeniería",
+    "Facultad de Medicina",
+    "Facultad de Medicina Veterinaria y Zootecnia",
+    "Facultad de Odontología",
+    "Facultad de Psicología",
+    "Facultad de Química",
 )
